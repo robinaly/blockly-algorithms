a4c580c0b32867933615dd6f6c8648d6adfa52d5
Robin Aly <robin@aly.de>
2016-09-19 14:11:33 +0200

    Reformatted languages

--- languages.js
+++ languages.js
@@ -2,28 +2,28 @@ function LanguagesModel() {
   var self = this;
   self.prefix = 'xyz';
   Model.call(self);
-  
+
   self.title("Blocky Little Man");
-  
+
   self.checkSucceeded = function() {
   }
-  
+
   self.javascript = ko.observable();
   self.php = ko.observable();
   self.python = ko.observable();
   self.lua = ko.observable();
   self.dart = ko.observable();
-  
+
   self.update = function(e) {
     Blockly.JavaScript.STATEMENT_PREFIX = '';
     var code = Blockly.JavaScript.workspaceToCode(self.workspace);
-    self.javascript(code);    
-    self.php(Blockly.PHP.workspaceToCode(self.workspace));    
+    self.javascript(code);
+    self.php(Blockly.PHP.workspaceToCode(self.workspace));
     self.python(Blockly.Python.workspaceToCode(self.workspace));
-    self.lua(Blockly.Lua.workspaceToCode(self.workspace));    
-    self.dart(Blockly.Dart.workspaceToCode(self.workspace));    
-  } 
-  
+    self.lua(Blockly.Lua.workspaceToCode(self.workspace));
+    self.dart(Blockly.Dart.workspaceToCode(self.workspace));
+  }
+
   self.localInit = function() {
     self.workspace.addChangeListener(self.update);
   }
@@ -33,5 +33,5 @@ LanguagesModel.prototype = Object.create(Model.prototype);
 LanguagesModel.prototype.init = function() {
   var self = this;
   Object.getPrototypeOf(LanguagesModel.prototype).init.call(this);
-  // 
+  //
 }
